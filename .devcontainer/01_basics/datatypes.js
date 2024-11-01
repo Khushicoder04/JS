007ec82ffe80448afcd8ca230f8983550a336984
Khushi Rai <155308071+Khushicoder04@users.noreply.github.com>
2024-11-01 14:19:44 +0000

    datatypes

---
+++ .devcontainer/01_basics/datatypes.js
@@ -0,0 +1,27 @@
+"use strict";//treat all js code as newer version
+// alert(3+3); we are using nodejs, not browser
+console.log(3+3)
+
+console.log("khushi");
+
+let name = "khushi";
+let age = 20
+let isloggedIn = false
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => Standalone value
+// Undefined =>
+    //  symbol => unique
+
+//objects 
+
+// console.log(typeof "khushi");
+
+// console.log(typeof "age");
+
+console.log(typeof undefined);
+
+console.log(typeof null);
---
+++ .devcontainer/01_basics/variable.js
@@ -0,0 +1,29 @@
+// console.log("Hello, World!");
+
+
+const accountId = 1445553
+let accountEmail = "khushi@google.com"
+var accountPassword = "12345"
+accountCity = "Hyd"
+let accountState;
+
+// accountId = 2;
+//curly baracket known as scope
+// if{
+  
+// } 
+accountEmail = "ansh@gmail.com"
+accountPassword = "112334"
+accountCity = "Banglore"
+
+console.log(accountEmail);
+
+console.log(accountId);
+/*
+prefer not to use var because of issue in
+block and functional scope
+*/
+
+console.table([accountId, accountEmail, accountCity, accountPassword, accountState]);
+
+

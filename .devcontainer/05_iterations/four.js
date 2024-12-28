7c6b9564ea943a2fdeb94ae9d249ee4cfda8202f
Khushi Rai <155308071+Khushicoder04@users.noreply.github.com>
2024-12-28 14:11:41 +0000

    four

---
+++ .devcontainer/05_iterations/four.js
@@ -0,0 +1,25 @@
+const myobject ={
+    js: 'javascript',
+    cpp: 'c++',
+    rb: 'ruby', 
+    swift: 'swift by object'
+}
+
+for (const key in myobject) {
+   // console.log(`${key} shortcut is for ${myobject[key]}`);
+}
+
+const programming = ["js", "c", "c++", "java"]
+
+for (const key in programming) {
+   // console.log(programming[key]);
+}
+
+// const map = new Map()
+// map.set('IN', "India")
+// map.set('USA', "United States")
+
+// for (const key in map) {
+//    console.log(key); 
+// }// this is not iterable
+

261e83bac5e679f869970a941326ee07a94ba901
Khushi Rai <155308071+Khushicoder04@users.noreply.github.com>
2024-12-28 15:21:40 +0000

    six

---
+++ .devcontainer/05_iterations/six.js
@@ -0,0 +1,31 @@
+// const coding = ["js", "c", "c++", "java", "python"]
+
+// const values = coding.forEach( (item) => {
+//     console.log(item);
+//     return item
+// })
+
+// console.log(values);// foreach will not return the value
+
+// const mynums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+// const newnums = mynums.filter( (nums) => nums>4)
+// console.log(newnums);
+
+const mynums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+// const newnums = mynums.filter( (num) => {
+//     return num>4
+// })
+// console.log(newnums);// this is giving only [] and to get the value we have to write return keyword in curly brackets
+
+// const newnums = []
+
+// mynums.forEach((num)=> {
+//     if(num>4){
+//         newnums.push(num)
+//     }
+// })
+
+// console.log(newnums);
+

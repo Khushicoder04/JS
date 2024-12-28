2b03209c7c4d1e7e10fb6af2931e9d35ae3beb7b
Khushi Rai <155308071+Khushicoder04@users.noreply.github.com>
2024-12-28 06:05:44 +0000

    switch

---
+++ .devcontainer/04_control_flow/switch.js
@@ -0,0 +1,50 @@
+// switch(key){
+//     case value:
+
+//     break;
+
+//     default:
+//         break;
+// }
+
+// const month = 3
+
+// switch (month) {
+//     case 1:
+//         console.log("jan");
+//         break;
+//     case 2:
+//          console.log("feb");
+//          break;
+//     case 3:
+//           console.log("mar");
+//           break;
+//           case 4:
+//             console.log("apr");
+//             break;      
+
+//     default:
+//         console.log("default case")
+//         break;
+// }
+
+const month1 = "mar"
+
+switch (month1) {
+    case "jan":
+        console.log("jan");
+        break;
+    case "feb":
+         console.log("feb");
+         break;
+    case "mar":
+          console.log("mar");
+          break;
+          case "apr":
+            console.log("apr");
+            break;      
+
+    default:
+        console.log("default case")
+        break;
+}
